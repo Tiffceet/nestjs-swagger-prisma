5a3136a10bac8ffe1e403ef6ca38c28467cf445f
Tiffceet <zkang.loo@gmail.com>
2024-04-04 20:19:43 +0800

    feat: add user table

--- src/generated/prisma-class/user.ts
+++ src/generated/prisma-class/user.ts
@@ -5,5 +5,14 @@ export class User {
   id: string;
 
   @ApiProperty({ type: String })
-  name: string;
+  username: string;
+
+  @ApiProperty({ type: String })
+  password: string;
+
+  @ApiProperty({ type: Date })
+  createdAt: Date;
+
+  @ApiProperty({ type: Date })
+  updatedAt: Date;
 }
